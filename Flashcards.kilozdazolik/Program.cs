6ed616a32743e9c4bdc93db1aa940cdb67770575
kilozdazolik <paulsonzoltan@protonmail.com>
2025-09-30 21:53:27 +0200

    feat: Initialize project structure and configuration
    - Created the base C# console application project.
    - Added `appsettings.json` to store configuration data, including the SQL Server connection string.

---
+++ Flashcards.kilozdazolik/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=localhost;Database=Flashcards;Trusted_Connection=True;TrustServerCertificate=True;";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection successful!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Connection failed:");
+        Console.WriteLine(ex.Message);
+    }
+}
